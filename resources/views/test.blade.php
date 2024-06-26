0fad3020c2c7359a389794b326195ad63bf5a754
hongha1806 <nguyenthihonghatchl@gmail.com>
2024-06-26 16:03:32 +0700

    test

---
+++ resources/views/test.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test</title>
+</head>
+<body>
+    @include('layouts.header')
+    <h1>Nội dung</h1>
+    @include('layouts.footer')
+</body>
+</html>
