d5a8ab440cfe028f90a4f634b7430f7b0fba7f8f
nhatnguyen-cr7 <nhat.nguyen-ojt07@devplus.edu.vn>
2024-06-26 16:13:04 +0700

    feat: ui login/register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,73 @@
 @extends('layouts.app')
-
+@push('style')
+<link rel="stylesheet" href="{{ asset('css/login.css') }}"> 
+@endpush
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+{{-- <style>
+    .custom-header {
+        font-size: 2rem;
+        font-weight: 900;
+    }
+    .custom-paragraph {
+        font-size: 1rem;
+        color: #A4A9B2;
+    }
+    .btn-custom {
+        background-color: #010E51;
+        border-color: #010E51;
+        color: white;
+        width: 100%;
+    }
+    .btn-custom:hover {
+        background-color: #3967C2;
+        border-color: #3967C2;
+        color: white;
+    }
+    .container-full {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .forgot-password {
+        /* text-align: start; */
+        margin-top: 10px;
+        font-size: 0.9rem;
+    }
+</style> --}}
+<div class="container-full">
+    <div>
+        <div class="custom-header">{{ __('Login') }}</div>
+        <p class="custom-paragraph">Please fill your detail to access your account.</p>
+        <form id="loginForm" method="POST" action="{{ route('login') }}" class="mb-3">
+            @csrf
+            <div class="mb-3">
+                <label for="Username" class="form-label">{{ __('Username') }}</label>
+                <input type="text" class="form-control " id="Username" name="Username" placeholder="Username" required>
+                <div class="invalid-feedback">
+                    Please enter a valid username.
                 </div>
             </div>
-        </div>
+            <div class="mb-3 position-relative">
+                <label for="password" class="form-label">{{ __('Password') }}</label>
+                <div class="input-group mb-3">
+                    <input type="password" class="form-control toggle-password" id="password" name="password" placeholder="Password" required>
+                    <span class="input-group-text toggle-password-icon">
+                        <i class="far fa-eye"></i>
+                    </span>
+                </div>
+                <div class="invalid-feedback">
+                    Please enter your password.
+                </div>
+            </div>
+            <div class="mb-3">
+                <a href="{{ route('password.request') }}" class="forgot-password">Forgot Password?</a>
+            </div>
+            <div class="mb-3">
+                <button type="submit" class="btn btn-custom">{{ __('Login') }}</button>
+            </div>
+        </form>
+        <div>Don’t have an account?<a href="{{ route('register') }}" class="text-decoration-none"> Register</a> </div>
+        
     </div>
 </div>
 @endsection
